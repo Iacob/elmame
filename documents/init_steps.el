83196c4b922141c471f942f53c3e06878c0d9928
Yong <luo.yong.name@gmail.com>
2023-04-06 22:02:09 +0800

    Add working-dir config entry.

--- documents/init_steps.el
+++ documents/init_steps.el
@@ -2,5 +2,7 @@
 (add-hook 'elmame-mame-mode-hook
 	  (lambda ()
 	    (setq elmame-mame-config
-		  '(rompath "roms2" args (-window "")))
+		  '(rompath "roms2"
+			    working-dir "/myhome"
+			    args (-window "")))
 	    ))
