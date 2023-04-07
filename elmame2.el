9f5893fcce33bf3be3ea0f73b98447a3ea79fb13
Yong <luo.yong.name@gmail.com>
2023-04-07 23:28:21 +0800

    Show all columns(name, year, manufacturer, desc)

--- elmame2.el
+++ elmame2.el
@@ -1,7 +1,8 @@
 
-(load-file "mame_machine_info_loader.el")
+;;(load-file "mame_machine_info_loader.el")
 
 (defun elmame-mame-get-config (name)
+  "Get config value with a name."
   (let ((default-config '(exec "mame" rompath "roms"))
 	config-value)
     (if (boundp 'elmame-mame-config)
@@ -11,15 +12,15 @@
     config-value ) )
 
 (defun elmame-mame-save-context (name value)
+  "Save value to elmame context with a name."
   (if (boundp 'elmame-mame-context)
       (if (assoc name elmame-mame-context)
-	  (setcdr (assoc name elmame-mame-context) (list value))
-	(nconc elmame-mame-context (list name value)) )
-    (setq elmame-mame-context (list (list name value)))
-    )
-  )
+	  (setcdr (assoc name elmame-mame-context) value)
+	(nconc elmame-mame-context (cons name value)) )
+    (setq elmame-mame-context (list (cons name value))) ) )
 
 (defun elmame-mame-list-roms ()
+  "Filter mame roms in rompath and return the list of valid ones."
   (let ((rompath (elmame-mame-get-config 'rompath))
 	filelist
 	machinelist
@@ -34,7 +35,7 @@
     machinelist ) )
 
 (defun elmame-mame-make-shell-command (machine-name)
-  ;; mame <machine-name> -rompath roms
+  
   (let ((exec (elmame-mame-get-config 'exec))
 	(rompath (elmame-mame-get-config 'rompath))
 	(args (elmame-mame-get-config 'args))
@@ -43,7 +44,7 @@
 	(setq args-text "")
       (setq args-text
 	    (string-join (mapcar (lambda (arg) (format "%s" arg)) args) " ")))
-    
+    ;; mame <machine-name> -rompath roms
     (format "%s %s -rompath %s %s" exec machine-name rompath args-text) ) )
 
 (defun elmame-mame ()
@@ -52,12 +53,19 @@
   (let (machinelist
 	column-width
 	(working-dir (elmame-mame-get-config 'working-dir))
-	fn-calc-width)
+	fn-calc-width
+	fn-get-width)
 
     (setq machinelist (elmame-mame-list-roms))
-    (setq fn-calc-width (lambda (col)
-			  (seq-max (mapcar (lambda (x) (length (plist-get x col))) machinelist))
-			  ) )
+    (setq fn-calc-width
+	  (lambda (col)
+	    (seq-max (mapcar (lambda (x) (length (plist-get x col))) machinelist)) ) )
+
+    (setq fn-get-width
+	  (lambda (col)
+	    (message "::%s" (alist-get 'column-width elmame-mame-context))
+	    (plist-get (alist-get 'column-width elmame-mame-context) col) ) )
+    
     (setq column-width
 	  (list 'name (funcall fn-calc-width 'name)
 		'year (funcall fn-calc-width 'year)
@@ -68,33 +76,48 @@
 
     (switch-to-buffer "**machine list**")
     (setq buffer-read-only nil)
+    (setq truncate-lines 't)
     (erase-buffer)
-    (when working-dir
-      (cd working-dir) )
     (when working-dir
       (message "Swtiching to directory: %s" working-dir)
       (cd working-dir) )
     (mapcar (lambda (m)
-	      (let ((machine-name (plist-get m 'name)))
+	      (let ((machine-name (plist-get m 'name))
+		    (year (plist-get m 'year))
+		    (manufacturer (plist-get m 'manufacturer))
+		    (desc (plist-get m 'desc))
+		    (name-width (funcall fn-get-width 'name))
+		    (year-width (funcall fn-get-width 'year))
+		    (manufacturer-width (funcall fn-get-width 'manufacturer))
+		    (desc-width (funcall fn-get-width 'desc))
+		    )
+		
 		(insert-button
 		 machine-name
-		 'action (lambda (x)
-			   ;;(message "%s" (button-get x 'machine-name))
-			   (let* ((machine-name (button-get x 'machine-name))
-				  (cmd-line (elmame-mame-make-shell-command machine-name)))
-			     ;;(message-box cmd-line)
-			     (switch-to-buffer-other-window "**mame output**")
-			     
-			     (insert (format "Running command: %s" cmd-line) "\n")
-			     (start-process-shell-command "mame command" "**mame output**" cmd-line)
-			     
-			     )
-			   )
+		 'action
+		 (lambda (x)
+		   (let* ((machine-name (button-get x 'machine-name))
+			  (cmd-line (elmame-mame-make-shell-command machine-name)))
+		     ;;(message-box cmd-line)
+		     (switch-to-buffer-other-window "**mame output**")
+		     
+		     (insert (format "Running command: %s" cmd-line) "\n")
+		     (start-process-shell-command "mame command" "**mame output**" cmd-line)
+		     
+		     )
+		   )
 		 'machine-name machine-name
 		 'follow-link 't)
+		
+		(insert (make-string (1+ (- name-width (length machine-name))) ? ))
+		(insert year)
+		(insert (make-string (1+ (- year-width (length year))) ? ))
+		(insert manufacturer)
+		(insert (make-string (1+ (- manufacturer-width (length manufacturer))) ? ))
+		(insert desc)
+		(insert (make-string (1+ (- desc-width (length desc))) ? ))
+		(insert "\n")
 		)
-	      
-	      (insert "\n")
 	      )
 	    machinelist)
     (beginning-of-buffer)
